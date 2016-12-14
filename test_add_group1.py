e28a56b8f56783b710529ac6c220ff2ac8c218ce
SCLT1975 <ncr_zeya@mail.ru>
2016-12-14 16:11:51 +0300

    Улучшил читаемость кода

--- test_add_group1.py
+++ test_add_group1.py
@@ -14,65 +14,65 @@ class test_add_group1(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_group1(self):
-
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_groups_page(wd)
-        self.create_new_group(wd)
-        self.return_to_groups(wd)
-        self.logout(wd)
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
+
 
     def return_to_groups(self, wd):
-        # return to group page
         wd.find_element_by_link_text("group page").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("2341")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("wafasdfsadf")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fhfghfdghfdgh")
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asdfasdf")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
+    def login(self, wd, username, password):
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+
+    def test_test_add_group1(self):
+
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_new_group(wd, name="2341", header="wafasdfsadf", footer="fhfghfdghfdgh")
+        self.return_to_groups(wd)
+        self.logout(wd)
+
+    def test_test_empty_group1(self):
+
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_new_group(wd, name="", header="", footer="")
+        self.return_to_groups(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
