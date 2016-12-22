774cd6f9a0236a325f04e3234c09a698e6ffbd4e
SCLT1975 <ncr_zeya@mail.ru>
2016-12-22 18:01:27 +0300

    выделен класс для управления сессиями

--- test/test_add_group1.py
+++ test/test_add_group1.py
@@ -14,15 +14,15 @@ def app(request):
 
 
 def test_test_add_group1(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_group(Group(name="2341", header="wafasdfsadf", footer="fhfghfdghfdgh"))
-    app.logout()
+    app.session.logout()
 
 
 def test_test_empty_group1(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
